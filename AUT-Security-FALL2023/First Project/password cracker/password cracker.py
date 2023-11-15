43c659b67f05d1e968e8779b7b3c07e46d3c7bcc
abtinz <abtinzand@gmail.com>
2023-11-15 17:32:51 +0330

    input class

--- AUT-Security-FALL2023/First Project/password cracker/password cracker.py
+++ AUT-Security-FALL2023/First Project/password cracker/password cracker.py
@@ -1,8 +1,20 @@
 import argparse
 
 search_modes = ['standard', 'first_character', 'this_known_k_latter']
-search_space = ['number', 'number_lowercase', 'lowercase', 'characters']
+search_space = ['number', 'number_lowercase', 'lowercase', 'full_space']
 
+class InputtedArguments():
+    def __init__(self, password, mode, space, password_length, first_character, k_char):
+       self.password = password
+       self.search_mode = mode
+       self.search_space = space
+       self.password_length = password_length
+       self.first_character = first_character
+       self.k_char = k_char
+
+
+def cracker_engine(inputted_arguments:InputtedArguments):
+   pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Password Cracker v.2000.turbo!')
@@ -16,5 +28,15 @@ if __name__ == '__main__':
     parser.add_argument('--k_char', type=str, nargs='+', help='If you have chosen (this_known_k_latter) mode, you must type k number of password letters')
    
     args = parser.parse_args()
-    password, mode, space, length_str, first_character, k_char =  args.password, args.mode, args.space, args.length, args.first_char, args.k_char
+
+    cracker_engine(
+        inputted_arguments= InputtedArguments(
+            password= args.password, 
+            mode=args.mode, 
+            space=args.space,
+            password_length= args.length, 
+            first_character=args.first_char, 
+            k_char=args.k_char
+        )
+    )
     
